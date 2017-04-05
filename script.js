cd38e3703900835535051db279d66e35703b3797
djs5435 <djs5435@mail.rit.edu>
2017-04-05 14:43:25 -0400

    Finished most of skills

--- script.js
+++ script.js
@@ -3,19 +3,9 @@ var canSwitch = true;
 $(document).ready(function(){
     $(".skill-content").hide();
 
-    $(".skill-front").mouseenter(function(){
-        $(".skill-content").slideDown();
+    $(".skill-front").click(function(){
+        $(this).siblings(".skill-content").toggle(1000);
     });
-    $(".skill-front").mouseleave(function(){
-        $(".skill-content").slideUp();
-    })
-    //$(".skill").mouseenter(skillToggle);
-    //$(".skill").mouseleave(skillToggle);
+
 
 });
-
-function skillToggle(){
-    $(this).find(".skill-content").toggle(1000);
-    //$(this).find(".skill-content").toggle(1000);
-
-}
