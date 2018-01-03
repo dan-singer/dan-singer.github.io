45e1758289fb1593e301364c2cfe164f32b3ae98
Dan Singer <dsinger.sax@gmail.com>
2018-01-03 17:11:36 -0500

    Create mobile nav system

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+let viewingNav = false;
+window.onload = function(e){
+    const burger = document.querySelector(".hamburger");
+    const navWindow = document.querySelector("nav > div:nth-child(2)");
+
+    burger.onpointerover = function(e){ setChildrenBGColor(this, "--tertiary-color");};
+    burger.onpointerleave = function(e){ setChildrenBGColor(this, "--white");};
+    burger.onclick = (e)=>{
+        navWindow.style.left = "-20%";
+        setTimeout(function(){ viewingNav = true; }, 1000);
+
+    };
+
+    document.onclick = function(e){
+        if (viewingNav){
+            navWindow.style.left = "-100%";
+            viewingNav = false;
+        }
+    };
+};
+
+function setChildrenBGColor(parent, colorVar){
+    for (let child of parent.children){
+        child.style.backgroundColor = getComputedStyle(parent).getPropertyValue(colorVar);
+    }
+}
