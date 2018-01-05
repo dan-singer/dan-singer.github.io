59c8de29618d654591754f766400ab88944e387a
Dan Singer <dsinger.sax@gmail.com>
2018-01-05 13:45:32 -0500

    Add PIXI

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 let viewingNav = false;
+let splash;
 const xmlns = "http://www.w3.org/2000/svg"
 window.onload = function(e){
     const burger = document.querySelector(".hamburger");
@@ -22,7 +23,10 @@ window.onload = function(e){
         }
     };
 
+
+    splash = document.querySelector("#splash-board");
     generateSplashSVG();
+
 };
 
 /**
@@ -31,8 +35,8 @@ window.onload = function(e){
  * @param {String} colorVar 
  */
 function setChildrenBGColor(parent, colorVar){
-    for (let child of parent.children){
-        child.style.backgroundColor = getComputedStyle(parent).getPropertyValue(colorVar);
+    for (let i=0; i<parent.children.length; i++){
+        parent.children[i].style.backgroundColor = getComputedStyle(parent).getPropertyValue(colorVar);
     }
 }
 
@@ -40,40 +44,49 @@ function setChildrenBGColor(parent, colorVar){
  * Generates an SVG particle effect for the splash screen
  */
 function generateSplashSVG(){
-        //Create splash animation
-        let splash = document.querySelector("#splash-board");
-        let size = {x:splash.clientWidth, y:splash.clientHeight};
-        const CIRCLE_COUNT = 100;
-        const SPEED = 1;
-        let colors = [getComputedStyle(document.documentElement).getPropertyValue("--white"), getComputedStyle(document.documentElement).getPropertyValue("--secondary-color"), getComputedStyle(document.documentElement).getPropertyValue("--tertiary-color")];
-        console.log(colors);
-        let radiusRange = {min: 1, max: 5};
-        for (let i=0; i<CIRCLE_COUNT; i++){
-            let circle = document.createElementNS(xmlns, "circle");
-            circle.setAttribute("cx", Math.random() * size.x);
-            circle.setAttribute("cy", Math.random() * size.y);
-            circle.setAttribute("r", radiusRange.min + Math.random() * (radiusRange.max - radiusRange.min));
-            circle.startY = parseFloat(circle.getAttribute("cy"));
-            circle.startTime = Math.random() * 1000;
-            let color = colors[parseInt(Math.random() * colors.length)];
-            circle.style.fill = color;
-            circle.style.transition = "fill 1s";    
+    //Create splash animation
+    const CIRCLE_COUNT = 100;
+    const SPEED = 1;
+    const colors = [getComputedStyle(document.documentElement).getPropertyValue("--white"), getComputedStyle(document.documentElement).getPropertyValue("--secondary-color"), getComputedStyle(document.documentElement).getPropertyValue("--tertiary-color")];
+    let radiusRange = {min: 1, max: 5};
+    for (let i=0; i<CIRCLE_COUNT; i++){
+        let circle = document.createElementNS(xmlns, "circle");
+        circle.setAttribute("cx", Math.random() * splash.clientWidth);
+        circle.setAttribute("cy", Math.random() * splash.clientHeight);
+        circle.setAttribute("r", radiusRange.min + Math.random() * (radiusRange.max - radiusRange.min));
+        circle.startY = parseFloat(circle.getAttribute("cy"));
+        circle.seed = Math.random() * 1000;
+        let color = colors[parseInt(Math.random() * colors.length)];
+        circle.style.fill = color;
+        circle.style.transition = "fill 1s";    
+
+        splash.appendChild(circle);
+    }
+    requestAnimationFrame(step);
     
-            splash.appendChild(circle);
+    /**
+     * Called each animation frame to animate the svg circles
+     * @param {Number} timestamp 
+     */
+    function step(timestamp){
+        for (let i=0; i<splash.children.length; i++){
+            let child = splash.children[i];
+            child.setAttribute("cx", parseFloat(child.getAttribute("cx")) + SPEED);
+            let yOff = Math.sin(child.seed + timestamp / 1000) * 50;
+            child.setAttribute("cy", child.startY + yOff);
+            if (child.getAttribute("cx") > splash.clientWidth + parseFloat(child.getAttribute("r"))){
+                child.setAttribute("cx",  -child.getAttribute("r"));
+            }
         }
         requestAnimationFrame(step);
-        
-        
-        function step(timestamp){
-            for (let child of splash.children){
-                child.setAttribute("cx", parseFloat(child.getAttribute("cx")) + SPEED);
-                let yOff = Math.sin(child.startTime + timestamp / 1000) * 50;
-                child.setAttribute("cy", child.startY + yOff);
-                if (child.getAttribute("cx") > size.x + parseFloat(child.getAttribute("r"))){
-                    child.setAttribute("cx",  -child.getAttribute("r"));
-                }
-            }
-            requestAnimationFrame(step);
+    }   
+
+    //Handle window resizing
+    window.addEventListener("resize", function(e){
+        for (let i=0; i<splash.children.length; i++){
+            let child = splash.children[i];
+            child.setAttribute("cx", Math.random() * splash.clientWidth);
+            child.setAttribute("cy", Math.random() * splash.clientWidth);
         }
-    
+    });
 }
